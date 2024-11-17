56c57433b018c2a000a0b104cc404cf521566109
beteab548 <beteabbaynessagne548@gmail.com>
2024-11-16 23:20:27 -0800

    updated the range input in the product detail configure to reflect real time data change

--- my-app/app/checkout/page.js
+++ my-app/app/checkout/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import classes from "./checkout.module.css";
 export default function CheckOut() {
   // here extract the qunatity and the price of products in the cart that is passed as a prop
@@ -7,7 +8,9 @@ export default function CheckOut() {
         <p>total Price: $ 1400</p>
       </div>
       <div>
+        <Link href={'/checkout'}>
         <button>Checkout</button>
+        </Link>
       </div>
     </div>
   );
